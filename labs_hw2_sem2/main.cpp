8604d9239e2edd795bb6bf17345b78234141bec5
olaf-bukowski <olaf.bukowski3210@gmail.com>
2023-03-21 20:22:10 +0100

    problem no1

---
+++ labs_hw2_sem2/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Time {
+public:
+    Time(int seconds = 0) : seconds_(seconds) {}
+
+    int getSeconds() const {
+        return seconds_;
+    }
+
+    void setSeconds(int seconds) {
+        seconds_ = seconds;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Time& t) {
+        int hours = t.seconds_ / 3600;
+        int minutes = (t.seconds_ % 3600) / 60;
+        int seconds = t.seconds_ % 60;
+        os << hours << "h:" << minutes << "m:" << seconds << "s";
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Time& t) {
+        int hours, minutes, seconds;
+        std::cout << "Enter the time in the format hh mm ss: ";
+        is >> hours >> minutes >> seconds;
+        t.seconds_ = hours * 3600 + minutes * 60 + seconds;
+        return is;
+    }
+
+    Time operator+(const Time& other) const {
+        return Time(seconds_ + other.seconds_);
+    }
+
+    Time operator-(const Time& other) const {
+        return Time(seconds_ - other.seconds_);
+    }
+
+    Time operator*(int scalar) const {
+        return Time(seconds_ * scalar);
+    }
+
+    operator int() const {
+        return seconds_;
+    }
+
+private:
+    int seconds_;
+};
+
+int main()
+{
+    Time t2(0);
+    cin >> t2;
+    cout << t2;
+    return 0;
+}
